3c4e1a37fc3cdd120fddd7a12a8ddf5f5903341d
Smoothiesss <ngquoccuong0498@gmail.com>
2020-04-18 19:54:03 +0700

    add datacontext

---
+++ Domain/Value.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+        public int Id { get; set; } // use as primary key
+
+        public string Name { get; set; }
+    }
+}
