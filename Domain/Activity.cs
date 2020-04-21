7ef8cf306562936fadd4b8007035623cd93d1cd2
Smoothiesss <ngquoccuong0498@gmail.com>
2020-04-22 00:48:30 +0700

    CRUD API

---
+++ Domain/Activity.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Domain
+{
+    public class Activity
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Address { get; set; }
+
+        public string Category { get; set; }
+        public string Venue { get; set; }
+
+
+    }
+}
--- Persistence/Migrations/20200421091928_ActivityEntityAdded.Designer.cs
+++ Persistence/Migrations/20200421091928_ActivityEntityAdded.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence;
 
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200421091928_ActivityEntityAdded")]
+    partial class ActivityEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -23,9 +25,6 @@ namespace Persistence.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<string>("Address")
-                        .HasColumnType("TEXT");
-
-                    b.Property<string>("Category")
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("Date")
